8d819ad03a3ec0c135574e96b482f69d4f4cfa52
Kirsten Keels <keelsk@gmail.com>
2020-12-21 15:10:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Write your solution here!
 const pets = ["Milo", "Otis", "Garfield"];
-const append = pets.push("Odie");
-const prepend = pets.unshift("Odie");
-const removeLast = pets.pop();
-const removeFirst = pets.shift();
+const append = pets;
+const prepend = pets;
+const removeLast = pets;
+const removeFirst = pets;
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
